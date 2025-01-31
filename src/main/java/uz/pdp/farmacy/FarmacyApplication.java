5bd9aca8b8116c4f76ec49f2248f2aef8dc170b6
Robiya <robiyaoffical0@gmail.com>
2025-01-31 09:19:04 +0500

    first commit

---
+++ src/main/java/uz/pdp/farmacy/FarmacyApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.farmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmacyApplication.class, args);
+    }
+
+}
